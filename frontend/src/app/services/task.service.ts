e7b3c7749d64cc41d6e4940adb6a2af9aeeed075
Mavrk1604 <mavrk1604@gmail.com>
2024-12-13 03:12:28 -0500

    fixed routing issues in tasks service

--- frontend/src/app/services/task.service.ts
+++ frontend/src/app/services/task.service.ts
@@ -8,23 +8,23 @@ import { Task } from '../models/task.model';
   providedIn: 'root'
 })
 export class TaskService {
-  private apiUrl = 'http://localhost:5000/api/get-tasks';
+  private apiUrl = 'http://localhost:5000/api';
   constructor(private http: HttpClient) { }
 
   getTasks(): Observable<Task[]> {
-    return this.http.get<Task[]>(this.apiUrl);
+    return this.http.get<Task[]>(`${this.apiUrl}/get-tasks`); // Calls /api/get-tasks
   }
 
   getTaskById(id: string): Observable<Task> {
-    return this.http.get<Task>(`${this.apiUrl}/tasks/${id}`);
+    return this.http.get<Task>(`${this.apiUrl}/get-task-by-id/${id}`); // Calls /api/get-task-by-id/:id
   }
 
   createTask(task: Task): Observable<Task> {
-    return this.http.post<Task>(`${this.apiUrl}/tasks`, task);
+    return this.http.post<Task>(`${this.apiUrl}/create-task`, task); // Calls /api/create-task
   }
 
   updateTask(id: string, task: Task): Observable<Task> {
-    return this.http.put<Task>(`${this.apiUrl}/tasks/${id}`, task);
+    return this.http.put<Task>(`${this.apiUrl}/update-task/${id}`, task); // Calls /api/update-task/:id
   }
 
 }
